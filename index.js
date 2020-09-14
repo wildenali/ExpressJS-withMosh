5a5a2a6c58740c6ffd1c3f54e8688d8773a8dace
wildenali <wildenwildenaliali@gmail.com>
2020-09-14 16:08:41 +0700

    Port dan process.env.PORT

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on PORT ${port}`));
